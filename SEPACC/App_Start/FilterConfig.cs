ed2e8201b7bbb3f04252a178a855262a5aaa83d5
Colm Carberry <ccarberry@live.com>
2019-03-05 18:42:51 +0000

    Add project files.

---
+++ SEPACC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SEPACC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
